3dae59247f9be21f60fcc2f38fe09b76422f87f7
ngochuyk812 <88924028+ngochuyk812@users.noreply.github.com>
2024-03-04 00:17:10 +0700

    init home page

---
+++ wwwroot/js/header.js
@@ -0,0 +1,9 @@
+﻿document.querySelector(".menu_mobile").addEventListener('click', (e) => {
+    e.preventDefault()
+    const elmMenu = document.querySelector(".menu_item_mobile")
+    if (elmMenu.style.display === "none") {
+        elmMenu.style.display = "flex"
+    } else {
+        elmMenu.style.display = "none"
+    }
+})
