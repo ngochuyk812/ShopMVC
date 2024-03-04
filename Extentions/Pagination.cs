83dc62e452ceb64274d33905412ea6fcf80917f4
ngochuyk812 <88924028+ngochuyk812@users.noreply.github.com>
2024-03-04 21:09:41 +0700

    render product data in the home page

---
+++ Extentions/Pagination.cs
@@ -0,0 +1,21 @@
+﻿namespace ShopMVC.Extentions
+{
+    public class Pagination<T> where T : class
+    {
+        public long Total { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public int TotalPage { get; set; }
+        public IEnumerable<T> Data { get; set; }
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPage;
+        public Pagination(long Total, int PageIndex, int PageSize, IEnumerable<T> Data)
+        {
+            this.Total = Total;
+            this.PageIndex = PageIndex;
+            this.PageSize = PageSize;
+            this.Data = Data;
+            TotalPage = (int)Math.Ceiling(Total / (double)PageSize);
+        }
+    }
+}
---
+++ Repositories/Interface/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using ShopMVC.Database;
+using ShopMVC.Database.Model;
+using ShopMVC.Extentions;
+using System.Linq.Expressions;
+
+namespace ShopMVC.Repositories.Interface
+{
+    public interface IRepository<T> where T : BaseModel
+    {
+        DbSet<T> DbSet { get; }
+        ShopContext DbContext { get; }
+        T Find(Expression<Func<T, bool>> filter);
+        Task<T> FindAsync(Expression<Func<T, bool>> filter);
+        Task<IEnumerable<T>> FindListAsync(Expression<Func<T, bool>> filter, Func<IQueryable<T>, IIncludableQueryable<T, object>> includeProperties = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null);
+        Task<Pagination<T>> PagiAsync(int index, int size, Expression<Func<T, bool>> filter, Func<IQueryable<T>, IIncludableQueryable<T, object>> includeProperties = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null);
+
+    }
+}
